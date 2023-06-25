8b0219244ecfece860a079e1b3e28614b1d03676
Amir Kalaki <amirm.kalaki@gmail.com>
2023-06-25 13:42:56 +0330

    1- Create a library for Forth functions
    KIForth.c -> main file
    forthLIB.h -> just declare functions
    forthLIB.c -> define the functions

--- SRC_CODE/forthLIB.c
+++ SRC_CODE/forthLIB.c
@@ -1,24 +1,39 @@
-//          >>>KIForth - Forth interpreter with C<<<
-
-//                  CREATED BY AMIR KALAKI
-
-//                      24 / JUNE / 2023
-
-//        COPYRIGHT © 2023 AMIR KALAKI. ALL RIGHTS RESERVED.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include "forthLIB.h"
 
-/*#define SIZE 150                    //Size of stack in GForth is 150
+#define SIZE 150                    //Size of stack in GForth is 150
+#define NULL ((void *)0)
 
 
 int top = -1 , STACK[SIZE];
 // It does not support spaces after :_ , ."_ , ... and upper or lower case and if a number is at the beginning of the expression
 char *keyWords[] = {".\"" , "(" , ")" , ":" , "." , ";" , "cr" , "dup" , "drop" , "swap" , "rot" , "+" , "-" , "*" , "/" , ">" , "<" , "=" , "and" , "or" , "xor" , "not" , "if" , "then" , "begin" , "until" , "do" , "loop" , ".s" , "/mod" , "mod" , "negate" , "nip" , "tuck" , "2swap" , "2dup" , "2over" , "2drop" , "s\""};
 char bye[] = "bye";
+
+
+
+
+
+void push(char *token)
+{
+    long int number;                 //The storage location of the number that atoi() returns
+    
+    //Oveflow checking
+    if (top == SIZE - 1)
+    {
+        printf("\n\n\t\t[-] STACK OVERFLOW!\n\n");
+    }
+    else
+    {
+        number = atoi(token);        //Adding numbers to the stack
+        top = top + 1;
+        STACK[top] = (int) number;
+    }
+}
+
 
 void ok() 
 {
@@ -105,18 +120,4 @@ void inputString()
     }
 }
 
-*/
-int main() {
 
-    printf ("\n\n\t\t>>>FORTH SATCK DEMO<<<\n");
-    for (int i = 0; i < 60; i++)
-    {
-        printf("-");
-    }
-    printf("\nKIForth version 1.0\nEnter the \"bye\" command to close the program.");
-    printf ("\n\n");
-    input:
-        inputString();
-    goto input;
-    
-}
---
+++ SRC_CODE/forthLIB.h
@@ -0,0 +1,26 @@
+//                >>>FORTH FUNCTIONS LIBRARY<<<
+
+//                  CREATED BY AMIR KALAKI
+
+//                      24 / JUNE / 2023
+
+//        COPYRIGHT © 2023 AMIR KALAKI. ALL RIGHTS RESERVED.
+
+
+
+
+
+
+
+//Push      :       Adds entered numbers to top of the stack
+
+void push(char *token);
+
+void ok();
+
+void printStack();
+
+void push(char *token);
+
+void inputString();
+
